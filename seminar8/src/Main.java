bdb8ce43ddc0e996ab37286d8ba9d037971c6857
SandaCocirla <sanda.cocirla@gmail.com>
2023-04-26 09:53:47 +0300

    Facade

---
+++ seminar8/src/Main.java
@@ -0,0 +1,13 @@
+import Clase.Facade;
+
+public class Main {
+    public static void main(String[] args) {
+            Facade facade = new Facade();
+            facade.interneazaPacient("Clase.Pacient 1");
+            facade.interneazaPacient("Clase.Pacient 1.5");
+            facade.interneazaPacient("Clase.Pacient 2");
+            facade.interneazaPacient("Clase.Pacient 3");
+            facade.interneazaPacient("Clase.Pacient 4");
+        }
+
+}
